cb2e9d4b3dc33b1efd076cab58917b80256fe3a5
Kathan Patel <41847677+kathan1509@users.noreply.github.com>
2025-01-06 13:47:05 +0530

    add express server setup with CORS and MongoDB connection

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log("Hello World this is Kathan");
+import { app } from "./app.js";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const PORT = process.env.PORT;
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection failed", error);
+    process.exit(1);
+  });
