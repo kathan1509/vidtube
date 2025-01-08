eabe0ee7a7e5f376bbe033b1a07f3e3a4434e397
Kathan Patel <41847677+kathan1509@users.noreply.github.com>
2025-01-08 19:28:12 +0530

    Add cookie parser, multer, cloudinary; implement aggregate pagination for videos/comments; enhance user model with password hashing & JWT

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -20,6 +21,7 @@ app.use(
 
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
+app.use(cookieParser());
 
 //Import Routes
 import healthcheckRouter from "./routes/healthcheck.routes.js";
